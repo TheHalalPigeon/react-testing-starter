e0aed12fc54ae0d3dc3420e44514ec00d8644972
Ayat Khaleque Akter <akhalequeakter@gmail.com>
2024-04-21 16:00:14 +0100

    Setting up Mock Service Worker

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,9 @@
 import { describe, expect, it } from "vitest";
 
 describe("group", () => {
-  it("should", () => {
-    expect(1).toBeTruthy();
+  it("should", async () => {
+    const res = await fetch("/categories");
+    const data = await res.json();
+    expect(data).toHaveLength(3);
   });
 });
